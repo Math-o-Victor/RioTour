b37c3bc73d19c6785b3451f23c3f01fe7bf22c1b
Bruna Franchi <bfzdev@gmail.com>
2022-08-30 11:30:49 -0300

    Repository e Controller Usuario

---
+++ src/main/java/com/expediciones/riotour/controller/UsuarioController.java
@@ -0,0 +1,60 @@
+package com.expediciones.riotour.controller;
+
+import com.expediciones.riotour.models.UsuarioModel;
+import com.expediciones.riotour.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RequestMapping("/usuario")
+@RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class UsuarioController {
+
+    @Autowired
+    public UsuarioRepository usuarioRepository;
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioModel>> getAll() {
+        return ResponseEntity.ok(usuarioRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioModel> getById(@PathVariable Long id) {
+        return usuarioRepository.findById(id)
+                .map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/titulo/{titulo}")
+    public ResponseEntity<List<UsuarioModel>> getByTitulo(@PathVariable String nome) {
+        return ResponseEntity.ok(usuarioRepository.findAllByNomeContainingIgnoreCase(nome));
+
+    }
+
+
+    @PostMapping
+    public ResponseEntity<UsuarioModel> post(@RequestBody @Valid UsuarioModel usuario) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuarioRepository.save(usuario));
+    }
+
+    @PutMapping
+    public ResponseEntity<UsuarioModel> put(@RequestBody @Valid UsuarioModel usuario) {
+        return usuarioRepository.findById(usuario.getId()).map(resp -> {
+            return ResponseEntity.ok().body(usuarioRepository.save(usuario));
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        return usuarioRepository.findById(id).map(resp -> {
+            usuarioRepository.deleteById(id);
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+}

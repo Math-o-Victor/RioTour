4977833ab3b73733c4f0b6a377aab90216e7541c
Fernanda <fernandalosneves@gmail.com>
2022-08-30 11:50:00 -0300

    Passageiro Repository e Controller criados

--- src/main/java/com/expediciones/riotour/repository/PassageiroRepository.java
+++ src/main/java/com/expediciones/riotour/repository/PassageiroRepository.java
@@ -2,6 +2,12 @@ package com.expediciones.riotour.repository;
 
 import com.expediciones.riotour.models.PassageiroModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface PassageiroRepository extends JpaRepository<PassageiroModel, Long> {
+	 public List<PassageiroModel> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 }

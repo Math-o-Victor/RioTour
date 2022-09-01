ca6841d7fb2682fbec3e0e1c354059a42253ce83
Bruna Franchi <bfzdev@gmail.com>
2022-09-01 12:26:06 -0300

    corrigindo permissão de usuario

--- src/main/java/com/expediciones/riotour/security/BasicSecurityConfig.java
+++ src/main/java/com/expediciones/riotour/security/BasicSecurityConfig.java
@@ -40,8 +40,8 @@ public class BasicSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
-                .antMatchers("/usuarios/logar").permitAll()
-                .antMatchers("/usuarios/cadastrar").permitAll()
+                .antMatchers("/usuario/logar").permitAll()
+                .antMatchers("/usuario/cadastrar").permitAll()
                 .antMatchers("/admin/logar").permitAll()
                 .antMatchers("/admin/cadastrar").permitAll()
                 .antMatchers(HttpMethod.OPTIONS).permitAll()

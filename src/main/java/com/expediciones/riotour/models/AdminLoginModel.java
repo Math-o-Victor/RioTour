70853f250b551951791c17b8b346df249de48c4e
Bruna Franchi <bfzdev@gmail.com>
2022-09-01 10:30:40 -0300

    adicionando security usuario-admin

---
+++ src/main/java/com/expediciones/riotour/models/AdminLoginModel.java
@@ -0,0 +1,34 @@
+package com.expediciones.riotour.models;
+
+public class AdminLoginModel {
+
+    private String email;
+
+    private String senha;
+
+    private String token;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
---
+++ src/main/java/com/expediciones/riotour/models/UsuarioLoginModel.java
@@ -0,0 +1,33 @@
+package com.expediciones.riotour.models;
+
+public class UsuarioLoginModel {
+    private String email;
+    
+    private String senha;
+
+    private String token;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
